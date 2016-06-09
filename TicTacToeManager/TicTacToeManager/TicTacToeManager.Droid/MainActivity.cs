79aa6cfc5a24f06a1853a1ab3acf3bd71480a525
benoit gidon <bgidon@axones.com>
2016-06-09 17:10:21 +0200

    prepared PCl, and cleaned android startup Activity

--- TicTacToeManager/TicTacToeManager/TicTacToeManager.Droid/MainActivity.cs
+++ TicTacToeManager/TicTacToeManager/TicTacToeManager.Droid/MainActivity.cs
@@ -12,22 +12,11 @@ namespace TicTacToeManager.Droid
 	[Activity (Label = "TicTacToeManager.Droid", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			SetContentView (Resource.Layout.Main);	
 		}
 	}
 }
---
+++ TicTacToeManager/TicTacToeManager/TicTacToeManager/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeManager
+{
+    public static class DataContext
+    {
+        public static bool Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Json
+        {
+            get;private set;
+        }
+        public static IReadOnlyList<User> Users { get; private set; }
+        public static IReadOnlyList<Game> Games { get; private set; }
+
+    }
+}
